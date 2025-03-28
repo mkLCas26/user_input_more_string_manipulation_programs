147213c98adf19b9bc5e7ab13d47ecf30bd410ec
Kristina Cas <m.kristinacas@gmail.com>
2025-03-28 23:51:11 +0800

    Code Written: check if string ends with a certain parameter -> single (final)

--- check_ending_no_endswith.py
+++ check_ending_no_endswith.py
@@ -8,3 +8,13 @@ ask for parameter to check
 print true or false
 """
 
+user = input("Enter word/s: ")
+checker = input("Character you would like to check at the end of the input: ")
+result = ""
+
+if user[-len(checker):] == checker:
+    result = True
+    print(f"Does it end with {checker}? {result}")
+else: 
+    result = False
+    print(f"Does it end with {checker}? {result}")
