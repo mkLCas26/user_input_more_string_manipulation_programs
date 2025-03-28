953c7d2e2b22de38dbf413746b2ced334e1ad8c3
Kristina Cas <m.kristinacas@gmail.com>
2025-03-28 16:31:52 +0800

    Added Pseudocode for removing beginning characters in string input

--- remove_beginning_charac_no_removeprefix.py
+++ remove_beginning_charac_no_removeprefix.py
@@ -1,3 +1,11 @@
 # B6 Prog02. Remove characs at the beginning of a string
 # w/o removeprefix(), replicate its function
 
+"""
+PSEUDOCODE
+
+user input
+use for loop to remove characs
+print output
+"""
+
