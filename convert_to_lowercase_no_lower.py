44339729e69c5b2960aa787c00e53fe92e5e2c31
Kristina Cas <m.kristinacas@gmail.com>
2025-03-28 20:50:04 +0800

    Add: dictionary for converting uppercase letters to lowercase letters

--- convert_to_lowercase_no_lower.py
+++ convert_to_lowercase_no_lower.py
@@ -10,6 +10,13 @@ for loop: checking if input is in lowercase
     print output
 """
 
+convert = {
+    "A": "a", "B": "b", "C": "c", "D": "d", "E": "e", "F": "f",
+    "G": "g", "H": "h", "I": "i", "J": "j", "K": "k", "L": "l",
+    "M": "m", "N": "n", "O": "o", "P": "p", "Q": "q", "R": "r",
+    "S": "s", "T": "t", "U": "u", "V": "v", "W": "w", "X": "x",
+    "Y": "y", "Z": "z"
+}
 
 user = input("Enter word/s: ")
 
