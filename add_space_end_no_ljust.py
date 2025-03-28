fef8119ff6fd0ea17802726d152a7a7e10d67806
Kristina Cas <m.kristinacas@gmail.com>
2025-03-29 00:22:51 +0800

    Try Code: first code draft (working) -> needs improvement on possible cases

--- add_space_end_no_ljust.py
+++ add_space_end_no_ljust.py
@@ -8,3 +8,12 @@ user input
 ask length wanted by the user
 print string
 """
+
+user = input("Enter word/s: ")
+width = int(input("How long do you want the string to be? "))
+
+if len(user) < width:
+    width -= len(user)
+    user += " " * width
+
+print(f"Result: '{user}'")
