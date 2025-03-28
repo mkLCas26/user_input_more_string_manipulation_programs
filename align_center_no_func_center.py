4c170295e154549f4e7f45349ae71d51a7f4ae16
Kristina Cas <m.kristinacas@gmail.com>
2025-03-29 00:38:45 +0800

    Added Pseudocode for aligning a string at the center of a given length

--- align_center_no_func_center.py
+++ align_center_no_func_center.py
@@ -1,2 +1,10 @@
 # B6 Prog07. Add space in the end at beginning 
 # To center string input w/o using center()
+
+"""
+PSEUDOCODE
+
+user input 
+ask user how long do they want the string to be
+print output
+"""
