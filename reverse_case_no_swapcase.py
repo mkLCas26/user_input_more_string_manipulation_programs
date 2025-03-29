fe474cc7a25bfd03083c6edf5d35eb7a52c4e2f2
Kristina Cas <m.kristinacas@gmail.com>
2025-03-29 08:40:25 +0800

    Added Pseudocode for reversing the case of string inputs

--- reverse_case_no_swapcase.py
+++ reverse_case_no_swapcase.py
@@ -1,1 +1,10 @@
 # B6 Prog08. Reverse the casing of each string charac w/o swapcase()
+
+"""
+PSEUDOCODE
+
+define dictionaries for upper to lower, lower to upper
+user input
+swapcases of input
+print output
+"""
