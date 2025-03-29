132656e5d8c77cb82c285afd6aa479618d789afb
Kristina Cas <m.kristinacas@gmail.com>
2025-03-30 00:26:01 +0800

    Add: to_upper dictionary, user input, and necessary variables

--- capital_every_first_letter_no_title.py
+++ capital_every_first_letter_no_title.py
@@ -9,3 +9,15 @@ user input
 make first letter of every word into capital letters
 print output
 """
+
+
+to_upper = {
+     "a": "A", "b": "B", "c": "C", "d": "D", "e": "E", "f": "F",
+     "g": "G", "h": "H", "i": "I", "j": "J", "k": "K", "l": "L",
+     "m": "M", "n": "N", "o": "O", "p": "P", "q": "Q", "r": "R",
+     "s": "S", "t": "T", "u": "U", "v": "V", "w": "W", "x": "X",
+     "y": "Y", "z": "Z"
+}
+
+user = input("Enter word/s: ")
+title_case = ""
