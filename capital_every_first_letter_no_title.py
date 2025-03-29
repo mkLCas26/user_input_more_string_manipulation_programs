ca0552e789cd1bd70b8ee19f17c103f348eed101
Kristina Cas <m.kristinacas@gmail.com>
2025-03-30 00:24:24 +0800

    Added Pseudocode for making the first letter of every word capitalize

--- capital_every_first_letter_no_title.py
+++ capital_every_first_letter_no_title.py
@@ -1,2 +1,11 @@
 # B6 Prog10. Make the first letter of every word capital
 # Don't use title()
+
+"""
+PSEUDOCODE 
+
+dictionary for uppercase
+user input
+make first letter of every word into capital letters
+print output
+"""
