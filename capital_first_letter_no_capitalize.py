ecae531d94a58afeb15b2455e8ca74808d46ef85
Kristina Cas <m.kristinacas@gmail.com>
2025-03-29 09:25:31 +0800

    Fix, Add: fixed pseudocode and added dictionary + user input

--- capital_first_letter_no_capitalize.py
+++ capital_first_letter_no_capitalize.py
@@ -5,7 +5,19 @@
 PSEUDOCODE
 
 define dictionary to make lowercase letters to upper
+user input
 make capitalization applicable to first word first letter only
 make the rest lowercase
 print output
 """
+
+to_upper = {
+     "a": "A", "b": "B", "c": "C", "d": "D", "e": "E", "f": "F",
+     "g": "G", "h": "H", "i": "I", "j": "J", "k": "K", "l": "L",
+     "m": "M", "n": "N", "o": "O", "p": "P", "q": "Q", "r": "R",
+     "s": "S", "t": "T", "u": "U", "v": "V", "w": "W", "x": "X",
+     "y": "Y", "z": "Z"
+ }
+
+user = input("Enter word/s: ")
+
