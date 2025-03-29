94b59e412962470fdb2ee6bf3b185e5fb2d86cde
Kristina Cas <m.kristinacas@gmail.com>
2025-03-29 09:21:31 +0800

    Added Pseudocode for capitalizing first letter and make the rest lowercase w/o using capitalize()

--- capital_first_letter_no_capitalize.py
+++ capital_first_letter_no_capitalize.py
@@ -1,3 +1,11 @@
 # B6 Prog09. Capitalize the first letter of string and lowercase the rest
 # Don't use capitalize()
 
+"""
+PSEUDOCODE
+
+define dictionary to make lowercase letters to upper
+make capitalization applicable to first word first letter only
+make the rest lowercase
+print output
+"""
