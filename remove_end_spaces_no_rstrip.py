bd1f5214446220be0fefb86687936e3ab5926fb2
Kristina Cas <m.kristinacas@gmail.com>
2025-03-30 17:46:15 +0800

    Try Code: first code trial -> removes space at the end but also removes the last character (non space)

--- remove_end_spaces_no_rstrip.py
+++ remove_end_spaces_no_rstrip.py
@@ -7,3 +7,16 @@ user input
 loop to remove spaces in the end
 print output
 """
+
+index = 0
+user = input("Enter word/s: ")
+
+for x in user:
+    if x == " ":
+        index -= 1
+    else:
+        break
+
+print(f"'{user[0:index]}'")
+        
+    
