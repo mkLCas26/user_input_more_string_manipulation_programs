1dd009cdfda395889c6f7726d213622764fd19aa
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 00:26:03 +0800

    Added Pseudocode for converting string into uppercase w/o using upper()

--- convert_to_uppercase_no_upper.py
+++ convert_to_uppercase_no_upper.py
@@ -1,2 +1,11 @@
 # B7 Prog03. Convert string characs into uppercase w/0 upper()
 
+"""
+PSEUDOCODE
+
+dictionary for alphabets
+user input and initialize needed variables
+loop to check if characs are in uppercase
+    convert if in lowercase
+print
+"""
