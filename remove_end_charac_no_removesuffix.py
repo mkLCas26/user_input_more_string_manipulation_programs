fc2b6b454f1d69068939f7ed724c6d7639adb30f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 00:19:44 +0800

    Code Written, Add: remove characters at the end w/o removesuffix() (final; added error message)

--- remove_end_charac_no_removesuffix.py
+++ remove_end_charac_no_removesuffix.py
@@ -8,3 +8,13 @@ ask charac/s that needs to be removed
 if statement for removing characs
 print 
 """
+
+user = input("Enter word/s: ")
+remove = input("Characters you want to remove at the end: ")
+
+if user.endswith(remove):
+    remlength = len(remove)
+    user = user[:remlength]
+    print(f"'{user}'")
+else:
+    print(f"ERROR!'{remove}' can't be found at the end of your string.")
