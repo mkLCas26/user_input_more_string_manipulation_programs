4e049ec17aae1186f7eb974574c98a59c1e66b39
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 00:58:56 +0800

    Try code: first draft -> code working but some inaccuracies when string has numbers which needs to be fixed

--- check_lowercase_no_islower.py
+++ check_lowercase_no_islower.py
@@ -7,3 +7,18 @@ user input and initialize needed variables
 check if characs are in lower case
 print
 """
+
+lower = "abcdefghijklmnop"
+result = ""
+user = input("Enter word/s: ")
+
+
+for letter in user:
+    if letter in lower or letter == " ":
+        result = True
+    elif letter.isalnum():
+        result = False
+    else: 
+        result = False
+
+print(f"The string is in lowercase? {result}")
