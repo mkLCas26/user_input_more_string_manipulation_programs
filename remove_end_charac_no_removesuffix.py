e42fcf59321853992db33cc2e1ad66b34840bc0c
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 00:13:09 +0800

    Added Pseudocode for removing characters at the end of a string w/o using removesuffix()

--- remove_end_charac_no_removesuffix.py
+++ remove_end_charac_no_removesuffix.py
@@ -1,1 +1,10 @@
 # B7 Prog02. Remove characs at the end of string w/o removesuffix()
+
+"""
+PSEUDOCODE
+
+user input 
+ask charac/s that needs to be removed
+if statement for removing characs
+print 
+"""
