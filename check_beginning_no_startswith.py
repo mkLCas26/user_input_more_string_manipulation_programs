f16b8f00e3bff0bb8c8ad83415d063a7578473ee
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 16:53:29 +0800

    Added Pseudocode for checking if a string input starts with a given parameter w/o using startswith()

--- check_beginning_no_startswith.py
+++ check_beginning_no_startswith.py
@@ -1,2 +1,11 @@
 # B7 Prog05. Check if string begins with characs with given parameter
 # Don't use startswith()
+
+"""
+PSEUDOCODE
+
+user input
+ask for characs that user wants to check in the beginning
+check if characs are at the beginning
+print True or False
+"""
