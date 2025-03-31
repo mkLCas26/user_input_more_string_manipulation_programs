279e82b0b40b8b88ce9e9bce931225359e58d0c9
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 22:14:58 +0800

    Code Written: Add 0 at the beginning of string according to user's parameters w/o zfill() (final)

--- add_zero_start_no_zfill.py
+++ add_zero_start_no_zfill.py
@@ -9,3 +9,12 @@ ask for length of string
 if statement to add zeros
 print
 """
+
+user = input("Enter number/s: ")
+width = int(input("How long do you want the string to be? "))
+
+if len(user) < width:
+    width -= len(user)
+    user = ("0" * width) + user
+    
+print(f"Result: '{user}'")
