3f8ed8d9aea212534419c6799338f8a2812f68f6
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 23:03:36 +0800

    Added Pseudocode for returning the index of given parameter by user w/o index()

--- return_loc_parameter_no_index.py
+++ return_loc_parameter_no_index.py
@@ -1,2 +1,11 @@
 # B7 Prog09. Return first loc of func parameter in string
 # Don't use index()
+
+"""
+PSEUDOCODE
+
+user input
+ask for word/charac that user want the location of
+find parameter's loc
+print index
+"""
