38c68dbf366408f8d9770f7f7afbe1df462b5d7f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 21:22:21 +0800

    Code Written: check if string starts with a given parameter (final)

--- check_beginning_no_startswith.py
+++ check_beginning_no_startswith.py
@@ -9,3 +9,14 @@ ask for characs that user wants to check in the beginning
 check if characs are at the beginning
 print True or False
 """
+
+user = input("Enter word/s: ")
+checker = input("Character/s you would like to check at the beginning of your input: ")
+result = "" 
+
+if user[:len(checker)] == checker:
+    result = True
+    print(f"Does it start with {checker}? {result}")
+else: 
+    result = False
+    print(f"Does it start with {checker}? {result}")
