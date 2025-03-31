c2ca0187e81a1f3af336d91516915ccc91a08cec
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 21:49:57 +0800

    Code Written: adding spaces at the beginning of a string to satisfy parameters w/o using rjust() (final)

--- add_space_start_no_rjust.py
+++ add_space_start_no_rjust.py
@@ -1,2 +1,20 @@
 # B7 Prog06. Add space at the beginning of string to satisfy 
 # required no. of characs. Don't use rjust()
+
+"""
+PSEUDOCODE 
+
+user input
+ask length of string preferred by the u
+if statement for adding spaces to reach length 
+print 
+"""
+
+user = input("Enter word/s: ")
+width = int(input("How long do you want the string to be? "))
+
+if len(user) < width:
+    width -= len(user)
+    user = (" " * width) + user
+
+print(f"Result: '{user}'")
