9fc58c8ab97213da9271580f6136f26db021cc0a
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 22:22:48 +0800

    Added Pseudocode for counting the number of times a parameter appears in user input w/o using count()

--- return_count_of_parameter_no_count.py
+++ return_count_of_parameter_no_count.py
@@ -1,2 +1,11 @@
 # B7 Prog08. Return how many times a function parameter appear 
 # in the string. Don't use count()
+
+"""
+PSEUDOCODE
+
+user input
+ask the word/s user wants to count
+determining the count of the parameter
+print count
+"""
