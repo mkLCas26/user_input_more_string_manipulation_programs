e9fe28067f02faa62641436beaea7df34b39c6e8
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 21:56:02 +0800

    Added Pseudocode for adding zeros to complete preferred string length w/o using zfill()

--- add_zero_start_no_zfill.py
+++ add_zero_start_no_zfill.py
@@ -1,2 +1,11 @@
 # B7 Prog07. Add 0 at the beginning of string to satisfy no. of characs
 # Don't use zfill()
+
+"""
+PSEUDOCODE
+
+user input
+ask for length of string
+if statement to add zeros
+print
+"""
