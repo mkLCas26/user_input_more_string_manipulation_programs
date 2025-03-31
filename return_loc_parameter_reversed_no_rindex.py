05981fbffcc90ec04adc3c01e27d07bb349190e9
Kristina Cas <m.kristinacas@gmail.com>
2025-04-01 00:02:52 +0800

    Try Code: first code trial -> problems with correctly reversing string so that iteration will start at the last character

--- return_loc_parameter_reversed_no_rindex.py
+++ return_loc_parameter_reversed_no_rindex.py
@@ -10,3 +10,11 @@ ask for word/charac that user want the location of
 find parameter's loc in string that is reversed
 print index
 """
+
+user = input("Enter word/s: ")
+parameter = input("Character/word to know the index in your input: ")
+
+for letter in range(len(user) - len(parameter) + 1, -1, -1):
+    if user[letter: letter + len(parameter)] == parameter:
+        print(f"Index is: {letter}")
+        break
