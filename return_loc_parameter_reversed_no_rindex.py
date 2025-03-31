efccad6ba25f468a86f52012318e5875a61c191f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-31 23:57:37 +0800

    Added Pseudocode for finding the index of parameter in a reversed string w/o using rindex()

--- return_loc_parameter_reversed_no_rindex.py
+++ return_loc_parameter_reversed_no_rindex.py
@@ -1,3 +1,12 @@
 # B7 Prog10. Return first loc of fun parameter in string 
 # starting from last charac
 # Don't use rindex()
+
+"""
+PSEUDOCODE
+
+user input
+ask for word/charac that user want the location of
+find parameter's loc in string that is reversed
+print index
+"""
